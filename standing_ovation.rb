9544fd4fa82d808320a710768d6209c2605a8e53
humbertotm <humberto.htm@gmail.com>
2018-03-28 15:17:45 -0700

    Working code for standing ovation. Some refactoring and clean up pending.

--- standing_ovation.rb
+++ standing_ovation.rb
@@ -1,3 +1,98 @@
+# Create a class that will process the input depending on the
+
+# Global vars and inputs
+# These shall be populated by the input
+$s_max = 5
+$audience_string = "110011"
+# Not ints yet
+$chars = $audience_string.split("")
+$people_to_invite = 0
+
+# print "#{$chars}\n"
+
+# validations
+if $audience_string.length != $s_max + 1
+  raise "Invalid input. Invalid length string."
+end
+
+if $audience_string[-1].to_i == 0
+  raise "Invalid input. String cannot end in 0."
+end
+
+=begin
+this one is to avoid repeating myself
+def calculate_standing_people(index)
+  counter = 0
+  people_standing = 0
+
+  loop do
+
+  end
+end
+=end
+
+def enough_people_standing(index)
+  counter = 0
+  people_standing = 0
+
+  if index == 0
+    return true
+  end
+
+  loop do
+    if counter < index
+      people_standing = people_standing + $chars[counter].to_i
+      counter = counter + 1
+    else
+      return people_standing >= index
+    end
+  end
+end
+
+def calculate_people_to_invite(index)
+  counter = 0
+  people_standing = 0
+  loop do
+    if counter < index
+      people_standing = people_standing + $chars[counter].to_i
+      counter = counter + 1
+      # print "counter: #{counter} people standing: #{people_standing}\n"
+    else
+      break
+    end
+  end
+
+  # print "We are outside the loop.\n"
+  people_to_invite = index - (people_standing + $people_to_invite)
+  return people_to_invite
+end
+
+
+
+$chars.each_with_index do |char, index|
+  # print "#{index} #{char}\n"
+  if char.to_i == 0
+    # print "Index #{index} with 0\n"
+    next
+  end
+
+  if enough_people_standing(index) == true
+    # print "Enough people standing for shyness level #{index}\n"
+    next
+  else
+    # print "Not enough people standing for shyness level #{index}\n"
+    additional_people_to_invite = calculate_people_to_invite(index)
+    # print "#{additional_people_to_invite}\n"
+    $people_to_invite = $people_to_invite + additional_people_to_invite
+  end
+end
+
+print "Persons to be invited: #{$people_to_invite}\n"
+
+
+
+
+
 =begin
 
 inputs and global vars
